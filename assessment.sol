4a9190071b3db0c9983c2c81fed0038de5d2d6b1
AinaUmpar <139347400+AinaUmpar@users.noreply.github.com>
2024-05-13 03:30:22 +0800

    Update assessment.sol

--- assessment.sol
+++ assessment.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract assessment {
+contract CustomBalance {
     uint256 public currentBalance;
     
     function ToDeposit(uint256 money) public {
